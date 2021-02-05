452b021e20e6da459d647edbfd28344cbd7208d4
Brianrahmarela <brian.rahmarela@gmail.com>
2021-02-05 09:05:15 +0700

    blm dapat hasilnya

---
+++ index.js
@@ -0,0 +1,20 @@
+//buat variable utk cek perhuruf
+let abjad = "abcdefghijklmnopqrstuvwxyz";
+
+function solve(s){
+  console.log(s);
+  //cek panjang param
+  var len = s.length;
+  //cek dari tengah
+  var mid = Math.floor(len/2);
+  console.log(mid);
+  console.log(mid);
+  //looping  data & cocokkan dgn var abjad
+  for ( var i = 0; i < mid; i++ ) {
+  
+  }
+
+  return true;
+  }
+
+solve('anna');
